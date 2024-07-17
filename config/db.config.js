745ab0174357fe643bc7de8e86f7d6d189cd1ed3
thirumuthammal <you@example.com>
2024-07-18 01:03:39 +0530

    chaged the docker compose yaml file

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose')
-require('dotenv/config')
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 /**
  * Db server connection
  */
-const URI = process.env. MY_DB
- mongoose.connect(URI, (err) => {
-    if(err){
-        console.log("DB Not Connected");
-    } else
-    console.log("DB Connected Successfully");
+const URI = process.env.MY_DB;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(URI, (err) => {
+  if (err) {
+    console.log("DB Not Connected");
+  } else console.log("DB Connected Successfully");
 });
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const dbconfig = require('./config/db.config');
-const cors = require('cors')
+const express = require("express");
+const dbconfig = require("./config/db.config");
+const cors = require("cors");
 
-const auth = require('./middlewares/auth');
-const errors = require('./middlewares/errors');
+const auth = require("./middlewares/auth");
+const errors = require("./middlewares/errors");
 
-const unless = require('express-unless');
+const unless = require("express-unless");
 
 const app = express();
-
-
 
 // auth.authenticateToken.unless = unless;
 
@@ -18,21 +16,21 @@ const app = express();
 //         path:[
 //             {url: "/users/register", methods: ["POST"]},
 //             {url: "/users/login", methods: ["POST"]},
-            
-//         ], 
+
+//         ],
 //     })
 // );
 
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-// app.options('*', cors());
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.options("*", cors());
 app.use(express.json());
 
 app.use("/users", require("./routes/users.routes"));
@@ -42,8 +40,8 @@ app.use(errors.errorHandler);
 /**
  * Localhost
  */
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, dbconfig, () => {
-    console.log('Server running on port:',PORT);
-})
+  console.log("Server running on port:", PORT);
+});
